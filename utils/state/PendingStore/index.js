18d5a273800285d5d9f4717a4fcd1c5b6a83ef43
Ryan O'Meara <ryan.omeara@versionone.dev>
2021-03-24 14:15:11 +0000

    Remove log from PendingStore

--- utils/state/PendingStore/index.js
+++ utils/state/PendingStore/index.js
@@ -5,7 +5,6 @@ export const PendingStore = {
   reducer: (state = [], action) => {
     // If not pending return undefined to clear the state
     const path = getType(action) + getKey(action);
-    console.log("Path", path);
     if (action.type.includes(statuses.PENDING) && !state.includes(path)) {
       return [...state, path];
     } else if (state.includes(path)) {
