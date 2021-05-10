6a64482b45bab4a2a120e00f426a39d4038b8c00
Ryan O'Meara <ryan.omeara@thisisdrum.com>
2021-05-10 09:23:18 +0100

    First draft of state docs

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: [
+    "../utils/**/*.stories.mdx",
+    "../utils/**/*.stories.js",
+    "../storybook/**/*.stories.js",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
