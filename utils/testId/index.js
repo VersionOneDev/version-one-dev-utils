e5e814edf9d7c175a61d5b708940a82f12122ea4
AdamJDuggan <adamduggan17@gmail.com>
2021-09-07 10:10:53 +0100

    added testId folder

---
+++ utils/testId/index.js
@@ -0,0 +1,12 @@
+const delimiter = "/";
+export const TestId = (props = {}) => {
+  const component = props["data-testid"];
+  return (element, key) => {
+    const testId = [component, element, key]
+      .filter((v) => v !== undefined)
+      .join(delimiter);
+    return {
+      "data-testid": testId,
+    };
+  };
+};
