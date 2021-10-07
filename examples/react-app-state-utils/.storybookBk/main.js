3aa18015b63ce789bdbd2b4716302861f1552384
Ryan O'Meara <ryan.omeara@thisisdrum.com>
2021-10-07 13:12:20 +0100

    Try to get tailwind working, move all util deps to top level

---
+++ examples/react-app-state-utils/.storybookBk/main.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-jest",
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\,css&/,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: [require("tailwindcss"), require("autoprefixer")],
+          },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
+};
--- examples/react-app-state-utils/src/index.js
+++ examples/react-app-state-utils/src/index.js
@@ -1,7 +1,7 @@
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 
-import "./index.css";
+import "./tailwind/output.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
