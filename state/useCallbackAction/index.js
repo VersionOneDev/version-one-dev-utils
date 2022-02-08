9ef4461f0715f43e8173324f42518cddf26c021e
Ryan O'Meara <ryan.omeara@thisisdrum.com>
2022-02-08 14:31:01 +0000

    Hide warning in useCallbackAction.js

--- state/useCallbackAction/index.js
+++ state/useCallbackAction/index.js
@@ -4,7 +4,8 @@ import { useEffect, useCallback } from "react";
  * Hook that automatically unsubscribes from a callback action when it's dependencies are updated or on unmount.
  **/
 export const useCallbackAction = (cb, deps) => {
-  const fn = useCallback(cb, deps);
+  // We only want to update fn when deps changes, not when cb changes as this will be generated in every render in the component.
+  const fn = useCallback(cb, deps); // eslint-disable-line
 
   useEffect(() => {
     let action;
